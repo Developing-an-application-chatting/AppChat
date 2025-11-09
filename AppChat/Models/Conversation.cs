ceda1bd2ebf8678e9ce732c2932ca8be85a06ec6
nkhoa <nkhoa3625@gmail.com>
2025-11-09 15:31:50 +0700

    Add models for app chat

---
+++ AppChat/Models/Conversation.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppChat.Models
+{
+    [Table("Conversations")]
+    public class Conversation
+    {
+        public int Id { get; set; }
+        public int User1Id { get; set; }
+        public int User2Id { get; set; }
+        public string LastMessage { get; set; } = string.Empty;
+        public DateTime LastMessageTime { get; set; }
+        public int UnreadCount { get; set; }
+    }
+}
---
+++ AppChat/Models/Friend.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppChat.Models
+{
+    [Table("Friends")]
+    public class Friend
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int FriendId { get; set; }
+    }
+}
