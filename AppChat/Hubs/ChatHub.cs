36ee89cd5720a0d126cf72f880dc1793de13bc1c
KaitoP1912 <thanhphuoc2797@gmail.com>
2025-11-10 22:44:47 +0700

    Add Hubs and new services (TokenService, UserService)

---
+++ AppChat/Hubs/ChatHub.cs
@@ -0,0 +1,70 @@
+﻿using AppChat.Data;
+using AppChat.Models.DTOs;
+using AppChat.Services;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly AppDbContext _context;
+        private readonly MessageService _msgService;
+
+        public ChatHub(AppDbContext context, MessageService service)
+        {
+            _context = context;
+            _msgService = service;
+        }
+
+        public string UserId => Context.UserIdentifier ?? "NULL";
+
+        public override Task OnConnectedAsync()
+        {
+            var user = Context.User; // Lấy thông tin người dùng từ JWT
+            if (user == null || !user.Identity.IsAuthenticated)
+            {
+                throw new UnauthorizedAccessException("Unauthorized");
+            }
+            return base.OnConnectedAsync();
+        }
+
+        public async Task SendMessage(string converId, string user, string message, string senderId)
+        {
+            // Lưu tin nhắn, lấy tên người gửi, và gửi tin nhắn đến các client
+            var msg = await _msgService.SaveMessage(converId, senderId, message);
+
+            var sender = await _context.Users
+                .Where(u => u.Id == int.Parse(senderId))
+                .FirstOrDefaultAsync();
+
+            if (sender != null)
+            {
+                var messageDto = new MessageDto
+                {
+                    Id = msg.Id,
+                    SenderId = msg.SenderId,
+                    SenderName = $"{sender.FirstName} {sender.LastName}",
+                    Content = msg.Content,
+                    SentTime = msg.SentAt.ToString("HH:mm:ss dd/MM/yyyy"),
+                    Status = msg.Status
+                };
+
+                await Clients.User(user).SendAsync("ReceiveMessage", messageDto);
+                await Clients.Caller.SendAsync("ReceiveMessage", messageDto);
+            }
+        }
+
+        public async Task UserTyping(string conversationId, string userId)
+        {
+            await Clients.OthersInGroup(conversationId)
+                .SendAsync("ReceiveTyping", conversationId, userId);
+        }
+
+        public async Task MessageSeen(string conversationId, string messageId)
+        {
+            await Clients.OthersInGroup(conversationId)
+                .SendAsync("MessageStatusUpdated", messageId, "seen");
+        }
+    }
+}
---
+++ AppChat/Services/UserService.cs
@@ -0,0 +1,111 @@
+﻿using AppChat.Data;
+using AppChat.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppChat.Services
+{
+    public class UserService
+    {
+
+        private readonly AppDbContext _context;
+
+        public UserService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> CreateUser(User newUser)
+        {
+            if (newUser == null)
+            {
+                throw new ArgumentNullException(nameof(newUser), "New user cannot be null");
+            }
+
+            try { 
+            
+                await _context.Users.AddAsync(newUser);
+
+                await _context.SaveChangesAsync();
+                return newUser;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error creating user: " + e.Message);
+            }
+        }
+
+        public async Task<User> GetUserById(int Id)
+        {
+            try
+            {
+                var user = await _context.Users.FindAsync(Id);
+                if (user == null)
+                {
+                    throw new KeyNotFoundException("User not found!");
+                }
+                return user;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error fetching user: " + e.Message);
+            }
+
+        }
+
+        public async Task<List<User>> GetAllUsersAsync()
+        {
+            try
+            {
+                return await _context.Users.ToListAsync();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error fetching all users: " + e.Message);
+            }
+        }
+
+        public async Task<bool> UpdateUser(int Id, User updatedUser)
+        {
+            try
+            {
+                var user = await _context.Users.FindAsync(Id);
+                if (user == null) throw new KeyNotFoundException("User not found!");
+
+                user.FirstName = updatedUser.FirstName;
+                user.LastName = updatedUser.LastName;
+                user.AvatarUrl = updatedUser.AvatarUrl;
+
+                await _context.SaveChangesAsync();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error updating user: " + e.Message);
+
+            }
+        }
+
+        public async Task<bool> DeleteUser(int Id)
+        {
+            try
+            {
+                var existingUser = await _context.Users.FindAsync(Id);
+                if (existingUser == null) throw new KeyNotFoundException("User not found!");
+
+                _context.Users.Remove(existingUser);
+                await _context.SaveChangesAsync();
+
+                return true;
+            }
+            catch(Exception e)
+            {
+                throw new Exception("Error deleting user: " + e.Message);
+            }
+            
+        }
+
+
+    }
+}
