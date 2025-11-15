6a8c70e9b705f2092a4772b08f6aeb4b4fae09fa
nkhoa <nkhoa3625@gmail.com>
2025-11-15 19:32:16 +0700

    Add Converter in Utils

---
+++ AppChat/Utils/ConnectionStringConverter.cs
@@ -0,0 +1,25 @@
+﻿namespace AppChat.Utils
+{
+    public static class ConnectionStringConverter
+    {
+        public static string ConvertConnectionString(string databaseUrl)
+        {
+            var uri = new Uri(databaseUrl);
+
+            var userInfo = uri.UserInfo.Split(':');
+            var username = userInfo[0];
+            var password = userInfo[1];
+
+            var host = uri.Host;
+            var port = uri.Port;
+            var database = uri.AbsolutePath.TrimStart('/');
+
+            return $"Host={host};" +
+                   $"Port={port};" +
+                   $"Username={username};" +
+                   $"Password={password};" +
+                   $"Database={database};" +
+                   $"SSL Mode=Require;Trust Server Certificate=true";
+        }
+    }
+}
