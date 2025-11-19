491105170774ce0d6d4d4d8bd31d365ac9a48ffa
nkhoa <nkhoa3625@gmail.com>
2025-11-19 18:03:23 +0700

    Modify: Check null, comment auth for local test

--- AppChat/Controllers/ContactController.cs
+++ AppChat/Controllers/ContactController.cs
@@ -1,7 +1,11 @@
-﻿using AppChat.Services;
+﻿using AppChat.Models;
+using AppChat.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace AppChat.Controllers
@@ -17,13 +21,29 @@ namespace AppChat.Controllers
             _service = service;
         }
 
+        //[Authorize]
         [HttpGet]  // https://localhost:5047/Contact
         public async Task<IActionResult> GetByUserId()
         {
             try
             {
-                int userId = 1; // TODO: Lấy từ token khi Authorize
+
+                // Lấy userId từ token
+                //var userIdClaim = User.FindFirstValue(ClaimTypes.NameIdentifier)    // Comment for testing
+                //    ?? User.FindFirstValue(JwtRegisteredClaimNames.Sub);
+
+                //if (!int.TryParse(userIdClaim, out int userId))
+                //    return BadRequest("Invalid user ID in token.");
+
+                int userId = 1;
+
                 var contactInfo = await _service.GetContactsByUserIdAsync(userId);
+                // Check if contacts are null?
+                if (contactInfo == null || !contactInfo.Any())
+                {
+                    return Ok(new List<Contact>());
+                }
+
                 return Ok(new { userId, contacts = contactInfo });
             }
             catch (Exception e)
