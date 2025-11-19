6ae72d90aee1b8b82f509e75023dc0cf53b65d80
nkhoa <nkhoa3625@gmail.com>
2025-11-19 22:08:04 +0700

    refactor: Add password property in User model, seeding data in DbContext

--- AppChat/Models/User.cs
+++ AppChat/Models/User.cs
@@ -9,6 +9,7 @@ namespace AppChat.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
+        public string Password { get; set; }
         public string AvatarUrl { get; set; } = string.Empty;
         public bool IsOnline { get; set; } = false;
         public DateTime LastSeen { get; set; }
